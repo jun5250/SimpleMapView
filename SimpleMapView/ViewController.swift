3a32fd777609f9f67094ae2f36945bd1da9aaddc
D7703_08 <d7703_08@D7703-08ui-iMac.local>
2018-06-07 11:30:00 +0900

    Initial Commit

---
+++ SimpleMapView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleMapView
+//
+//  Created by D7703_08 on 2018. 6. 7..
+//  Copyright © 2018년 D7703_08. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
